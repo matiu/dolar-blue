90f87a1de47a063c3ae023c10fbeb852dda0ad72
Matias Alejo Garcia <ematiu@gmail.com>
2013-12-16 19:31:30 -0300

    first working release

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports = require('./lib/dolar-blue').getData;
+
---
+++ test/basic.js
@@ -0,0 +1,29 @@
+var assert = require("assert")
+var dolarblue = require('../lib/dolar-blue');
+
+describe('exchange rates from dolar-blue.net', function(){
+
+  
+  
+    describe('getData', function(){
+
+        
+        it('should return current dolar blue BUY and SELL cotization, an datetime', function(done){
+            dolarblue.getData(function(data) {
+                assert.ok( data.buy > 0);
+                assert.ok( data.sell > 0);
+
+
+                var d = new Date(data.datetime);
+                console.log(d);
+                console.log(d > new Date(0));
+                var before =  new Date('2013-12-16 18:00:00');
+                assert.ok( d > before);
+                done();
+            });
+        })
+ 
+  })
+})
+
+
